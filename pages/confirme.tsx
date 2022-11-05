81e7d648c57da369f610ef3ed41aef42025ccde0
Bahmed Benyammi <40165601+bahmedBenyammi@users.noreply.github.com>
2022-11-06 00:21:51 +0100

    Update confirme.tsx

--- pages/confirme.tsx
+++ pages/confirme.tsx
@@ -9,7 +9,7 @@ import {FacebookIcon, FacebookShareButton} from "react-share";
 interface PropsConfirme{
     country:string
 }
- const Confirme: NextPage<PropsConfirme> = () => {
+ const Confirme: NextPage<PropsConfirme> = ({country}) => {
 
     return(
         <>
@@ -22,10 +22,10 @@ interface PropsConfirme{
                    <p className='rotate-6 text-3xl font-bold'>!</p>
                </div>
                <p className="text-xl font-bold">you support in world cup 2022</p>
-               {/*<Flag className='h-40 w-40' country={Short[country.replace(" ","_")]} />*/}
-               <Flag className='h-40 w-40' country={Short['DZ']} />
-               {/*<p className="text-xl m-2 font-bold">{country}</p>*/}
-               <p className="text-xl m-2 font-bold">Algeria</p>
+               <Flag className='h-40 w-40' country={Short[country.replace(" ","_")]} />
+              {/* <Flag className='h-40 w-40' country={Short['DZ']} />*/}
+              <p className="text-xl m-2 font-bold">{country}</p>
+                {/*<p className="text-xl m-2 font-bold">Algeria</p>
                <div className='flex justify-center items-center'>
                    <p>Share in :</p>
                    <FacebookShareButton url={ 'https://footballsuppurters.com'} quote="hi" contextMenu={'dd'}
@@ -34,29 +34,29 @@ interface PropsConfirme{
 
                        <FacebookIcon size={32} round={true}></FacebookIcon>
                    </FacebookShareButton>
-               </div>
+               </div>*/}
                <p className="m-2 font-bold">consulte the global ranking from
                    <Link href={'/'} ><span className="text-blue-500 hover:cursor-pointer"> here</span></Link>
                </p>
            </div>
     </div></>)
 }
-// export const getServerSideProps: GetServerSideProps = async ({query}) => {
-//     let {email,id}=query
-//     let redirect={
-//         redirect: {
-//             permanent: false,
-//             destination: "/404",
-//         }}
-//     if (!email||!id)
-//         return redirect
-//     let confirme=await confirmeGlobal(email as string, id as string)
-//     console.log(confirme)
-//     if (confirme==='not found')
-//         return redirect
-//     return {
-//         props:{country:confirme}
-//     }
-// }
+export const getServerSideProps: GetServerSideProps = async ({query}) => {
+    let {email,id}=query
+    let redirect={
+        redirect: {
+            permanent: false,
+            destination: "/404",
+        }}
+    if (!email||!id)
+        return redirect
+    let confirme=await confirmeGlobal(email as string, id as string)
+    console.log(confirme)
+    if (confirme==='not found')
+        return redirect
+    return {
+        props:{country:confirme}
+    }
+}
 
 export default Confirme;
