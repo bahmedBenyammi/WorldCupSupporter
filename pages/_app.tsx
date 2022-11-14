d2afdf1b458fed3e20f28fc54b7dc28ca5e04c40
Bahmed <benyammibahmed@gmail.com>
2022-11-14 16:27:27 +0100

    building ti,eout error resolved

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,32 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import BottomPage from "../components/BottomPage";
+import Script from "next/script";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  return <div >
+  return<>
+    <Script strategy="afterInteractive"  async src="https://www.googletagmanager.com/gtag/js?id=G-4QXLZBF9CQ"/>
+    <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-4QXLZBF9CQ', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+    />
+  <div >
     <Component {...pageProps} />
     <BottomPage/>
 
-    </div>
+    </div ></>
 }
 
 export default MyApp
