c8edd6f0af6ff57494f41059293688e8a42801ab
Bahmed <benyammibahmed@gmail.com>
2022-12-04 16:36:48 +0100

    most done

--- components/domaisList.ts
+++ components/domaisList.ts
@@ -2,9 +2,8 @@ const domainList:string[]=['gmail.com','outlook.com','hotmail.com','aim.com','ya
     'pm.com','zoho.com','yandex.com','gmx.com','titan.email','hubspot.com','tutanota.com']
 
 export const checkDomain=(email:string)=>{
-    console.log(email)
     let index=email.indexOf('@')
     let domain=email.substring(index+1)
-    console.log(domain)
+
   return domainList.some(e=>e===domain)
 }
